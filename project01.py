9c938a3019cd44d0113be163aec524b1b7541a60
Raphael Henrique <raphael.hkiller@gmail.com>
2025-10-31 23:57:13 -0300

    .

--- project01.py
+++ project01.py
@@ -1,9 +1,3 @@
-#Crie um programa que leia dois valores e mostre um menu na tela
-#[1]somar
-#[2]multiplicar
-#[3]maior
-#[4]novos números
-#[5]sair do programa   
 n1 = int(input("Primeiro valor: "))
 n2 = int(input("Segundo valor: "))
 
