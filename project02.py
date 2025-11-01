f664928445516622b54b8c395ea01859093162ca
Raphael Henrique <raphael.hkiller@gmail.com>
2025-10-31 23:57:27 -0300

    .

--- project02.py
+++ project02.py
@@ -1,7 +1,3 @@
-#Desenvolva um programa que leia o nome,idade e sexo de 4 pessoas. No final do programa, mostre:
-#A média de idade do grupo
-#Qual é o nome do homem mais velho
-#Quantas mulheres têm menos de 20 anos  
 somaidade = 0
 mediaidade = 0
 maior_idadehomem = 0
