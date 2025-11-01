7f023c37a9f178d77a02905223a6818637d0b326
Raphael Henrique <raphael.hkiller@gmail.com>
2025-10-31 23:44:44 -0300

    .

--- ex011.py
+++
@@ -1,6 +0,0 @@
-L = float(input('Largura da parede:'))
-A = float(input('Altura da parede:'))
-Area = L * A
-T = Area / 2 
-print('Sua parede tem a dimensão de {}X{} e sua área é de {}m2'.format(L, A, Area))
-print('Para pintar essa parede você precisará de {}t de tinta'.format(T))          
